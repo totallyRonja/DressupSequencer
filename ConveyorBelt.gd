ce75cdfcc66c9733e85f547fe7dfb3e66c85559d
Simon Robl <simon.robl@live.de>
2020-02-02 11:40:51 +0100

    push this for ronja to fix
    Im a lazy fuck

--- ConveyorBelt.gd
+++ ConveyorBelt.gd
@@ -5,15 +5,13 @@ class_name BeltMover
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var children = []
+
 export var pixelsPerSecond = 200;
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	children = get_children();
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	for child in children:
+	for child in get_children():
 		child.position += Vector2(0,pixelsPerSecond * delta);
+		if child.global_position.y > 780:
+			child.position = Vector2(0,-100)
